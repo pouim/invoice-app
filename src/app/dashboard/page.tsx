960a7491cd3bb3df1fa490592202e34b54b1578c
Pouyan Ahmadpour <ahmadpour.pouyan@gmail.com>
2025-01-30 21:00:27 +0100

    feat: show invoices in the table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,8 +11,12 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { CirclePlus } from "lucide-react";
 import Link from "next/link";
+import { db } from "@/db";
+import { Invoices } from "@/db/schema";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const results = await db.select().from(Invoices);
+
   return (
     <main className="flex flex-col justify-center h-full gap-6 text-center max-w-5xl mx-auto">
       <div className="flex justify-between">
@@ -37,23 +41,33 @@ export default function Dashboard() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="text-left font-medium p-4">
-              <span className="font-semibold">10/31/2024</span>
-            </TableCell>
-            <TableCell className="text-left p-4">
-              <span className="font-semibold">Pouyan Ahmadpour</span>
-            </TableCell>
-            <TableCell className="text-left p-4">
-              <span>ahmadpour.pouyan@gmail.com</span>
-            </TableCell>
-            <TableCell className="text-center p-4">
-              <Badge className="rounded-full">Open</Badge>
-            </TableCell>
-            <TableCell className="text-right p-4">
-              <span className="font-semibold">$250.00</span>
-            </TableCell>
-          </TableRow>
+          {results.map(({ id, createTs, value, status }) => (
+            <TableRow key={id}>
+              <TableCell className="text-left font-medium p-4">
+                <Link href={`/invoices/${id}`} className="font-semibold">
+                  {new Date(createTs).toLocaleDateString()}
+                </Link>
+              </TableCell>
+              <TableCell className="text-left p-4">
+                <Link href={`/invoices/${id}`} className="font-semibold">
+                  Pouyan Ahmadpour
+                </Link>
+              </TableCell>
+              <TableCell className="text-left p-4">
+                <Link href={`/invoices/${id}`}>ahmadpour.pouyan@gmail.com</Link>
+              </TableCell>
+              <TableCell className="text-center p-4">
+                <Link href={`/invoices/${id}`}>
+                  <Badge className="rounded-full">{status}</Badge>
+                </Link>
+              </TableCell>
+              <TableCell className="text-right p-4">
+                <Link href={`/invoices/${id}`} className="font-semibold">
+                  $ {(value / 100).toFixed(2)}
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </main>
