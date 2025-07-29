86e8058d59cb90e6343af1d9deb84c9fcb3eca06
Pouyan Ahmadpour <ahmadpour.pouyan@gmail.com>
2025-07-29 10:07:05 +0200

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -5,3 +5,11 @@ import NextError from "next/error";
 export default function error({ error }: { error: Error }) {
   return <NextError statusCode={500} title={error.message} />;
 }
+d
+a\d
+ad
+\a
+d\a
+d\a
+\d
+aa
